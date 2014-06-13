e6bb90af612929007c060ed903b2b3a9ee7f587b
Andrew Nash <akahadaka@gmail.com>
2014-06-13 14:03:09 +0200

    Initial commit

---
+++ puppet/manifests/main.pp
@@ -0,0 +1,48 @@
+stage { 'prepare': 
+	before => Stage['main'] 
+}
+
+class {
+	'bootstrap':      stage => prepare;
+	'tools':          stage => main;
+	'php':            stage => main;
+	'nginx':          stage => main;
+	'mysql':          stage => main;
+	'composer':       stage => main;
+	'phalcon':        stage => main;
+}
+
+addServer {'phalcon':
+	site => 'phalcon.dev',
+	root => '/vagrant/www/'
+}
+
+addServer {'media':
+	site => 'media.dev',
+	root => '/vagrant/www/media.dev/public/'
+}
+
+addServer {'jink':
+	site => 'jink.dev',
+	root => '/vagrant/www/jink.dev/public/'
+}
+
+addServer {'panda.admin':
+	site => 'admin.ponderingpanda.dev',
+	root => '/vagrant/www/admin.ponderingpanda.dev/public/'
+}
+
+addMysql { 'jink_db':
+    user => 'jink_usr',
+    password => 'jink_pass'
+}
+
+addMysql { 'pp_admin_db':
+    user => 'pp_admin_user',
+    password => 'iPMk4Qmc'
+}
+
+addMysql { 'pp_app_db':
+    user => 'pp_app_user',
+    password => 'SXnkpXrF'
+}
